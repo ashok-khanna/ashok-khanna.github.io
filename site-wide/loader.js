3297a0177b46139bda364fd5ace2e74f79f25f76
Ashok Khanna <46754667+ashok-khanna@users.noreply.github.com>
2020-09-27 03:49:32 +0400

    Create loader.js

---
+++ site-wide/loader.js
@@ -0,0 +1,18 @@
+$( "#sidebar-menu" ).load( "/helpers/sidebar-menu.html", function() {
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var contenta = this.nextElementSibling;
+    if (contenta.style.display === "block") {
+      contenta.style.display = "none";
+    } else {
+      contenta.style.display = "block";
+    }
+  });
+}
+});
+  
+$( "#site-footer" ).load( "/site-wide/footer.html" );
