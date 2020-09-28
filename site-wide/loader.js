2cb61e126d015bdc01baca9a4963e4453259dabb
Ashok Khanna <46754667+ashok-khanna@users.noreply.github.com>
2020-09-28 21:32:57 +0400

    Update loader.js

--- site-wide/loader.js
+++ site-wide/loader.js
@@ -26,6 +26,9 @@ for (i = 0; i < coll.length; i++) {
 
 $( "#site-header" ).load( "/site-wide/header.html" );
 $( "#site-footer" ).load( "/site-wide/footer.html" );
-$( "#page-content" ).load( contentFile );
+$( "#page-content" ).load( contentFile function() {
+  MathJax.typeset();
+  
+});
 
 });
