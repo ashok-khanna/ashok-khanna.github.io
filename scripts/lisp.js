9428d379d8e334d2cef4226dbbd017c73cad3fdb
ashok-khanna <46754667+ashok-khanna@users.noreply.github.com>
2021-05-21 15:39:43 +0400

    Update lisp.js

--- scripts/lisp.js
+++ scripts/lisp.js
@@ -1,10 +1,13 @@
+// https://stackoverflow.com/questions/62857743/how-to-copy-to-clipboard-text-from-code-and-pre-tags-using-jquery-and-js
+
 $(document).ready(function() {
   $('code').append('<button class="copy-btn">Copy to Clipboard</button>');
 
   $('code button.copy-btn').click(function(e) {
-    var text = $(this).parent().first().text().trim(); //.text();
+    var text = $(this).parent().text(); //.text();
+    var newText = text.substring(0, text.length - 18);
     var copyHex = document.createElement('input');
-    copyHex.value = text
+    copyHex.value = newText;
     document.body.appendChild(copyHex);
     copyHex.select();
     document.execCommand('copy');
