0db8394f98198303c951575249af2ec858a660eb
ashok-khanna <46754667+ashok-khanna@users.noreply.github.com>
2021-05-21 15:53:20 +0400

    Update lisp.js

--- scripts/lisp.js
+++ scripts/lisp.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
   $('code button.copy-btn').click(function(e) {
     var text = $(this).parent().text(); //.text();
     var newText = text.substring(0, text.length - 18);
-    var copyHex = document.createElement('input');
+    var copyHex = document.createElement('input').css('white-space: pre-line');
     copyHex.value = newText;
     document.body.appendChild(copyHex);
     copyHex.select();
@@ -15,19 +15,3 @@ $(document).ready(function() {
     document.body.removeChild(copyHex);
   });
 })
-
-
-function myFunction() {
-  /* Get the text field */
-  var copyText = document.getElementById("myInput");
-
-  /* Select the text field */
-  copyText.select(); 
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-  /* Copy the text inside the text field */
-  document.execCommand("copy");
-
-  /* Alert the copied text */
-  alert("Copied the text: " + copyText.value);
-}
