a265260d2aca6245da6765ee2cc05cd8dcee268f
ashok-khanna <46754667+ashok-khanna@users.noreply.github.com>
2021-05-21 13:53:04 +0400

    Update

---
+++ scripts/lisp.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  $('code, pre').append('<span class="command-copy" ><i class="fa fa-clipboard" aria-hidden="true"></i></span>');
+
+  $('code span.command-copy').click(function(e) {
+    text = $(this).parent().select(); //.text();
+    copiedText = $.trim(text);
+    document.execCommand("copy");
+  });
+
+
+  $('pre span.command-copy').click(function(e) {
+    text = $(this).parent().parent().select(); //.text();
+    copiedText = $.trim(text);
+    document.execCommand("copy");
+  });
+})
+
+
+function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  /* Select the text field */
+  copyText.select(); 
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  document.execCommand("copy");
+
+  /* Alert the copied text */
+  alert("Copied the text: " + copyText.value);
+}
