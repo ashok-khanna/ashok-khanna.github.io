53e2819e68c2d9dc90e7508fd092771fa0c24662
ashok-khanna <46754667+ashok-khanna@users.noreply.github.com>
2021-05-21 16:07:46 +0400

    Update lisp.js

--- scripts/lisp.js
+++ scripts/lisp.js
@@ -14,6 +14,6 @@ $(document).ready(function() {
     copyHex.select();
     document.execCommand('copy');
     console.log(copyHex.value)
-    document.body.removeChild(copyHex);
+    // document.body.removeChild(copyHex);
   });
 })
